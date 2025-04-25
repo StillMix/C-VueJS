6c76ce907ca5c7d79d8dc1ed2f4859920bc30d3c
roman_baranchikov <roman_barachikov@onr-kursk.ru>
2025-04-25 13:44:00 +0300

    new upload

--- cpp-server/src/drawings_handler.h
+++ cpp-server/src/drawings_handler.h
@@ -6,13 +6,45 @@
 #include <QWebEngineUrlRequestJob>
 #include <QFile>
 #include <QMimeDatabase>
+#include <QDebug>
 
 class DrawingsSchemeHandler : public QWebEngineUrlSchemeHandler {
     Q_OBJECT
 public:
-    explicit DrawingsSchemeHandler(QObject *parent = nullptr) : QWebEngineUrlSchemeHandler(parent) {}
+    explicit DrawingsSchemeHandler(QObject *parent = nullptr) : QWebEngineUrlSchemeHandler(parent) {
+        qDebug() << "DrawingsSchemeHandler создан";
+    }
     
-    void requestStarted(QWebEngineUrlRequestJob *job) override;
+    void requestStarted(QWebEngineUrlRequestJob *job) override {
+        QUrl url = job->requestUrl();
+        QString fileName = url.path();
+        
+        qDebug() << "Запрос к drawings://" << fileName;
+        
+        // Удаляем начальный слеш
+        if (fileName.startsWith("/")) {
+            fileName = fileName.mid(1);
+        }
+        
+        // Формируем путь к файлу
+        QString filePath = QDir::current().filePath("drawings/" + fileName);
+        qDebug() << "Полный путь к файлу:" << filePath;
+        
+        QFile *file = new QFile(filePath, job);
+        if (!file->open(QIODevice::ReadOnly)) {
+            qWarning() << "Ошибка открытия файла:" << file->errorString();
+            job->fail(QWebEngineUrlRequestJob::UrlNotFound);
+            return;
+        }
+        
+        // Определяем MIME-тип файла
+        QMimeDatabase db;
+        QMimeType mime = db.mimeTypeForFile(filePath);
+        qDebug() << "Отправка файла с MIME-типом:" << mime.name();
+        
+        // Отправляем файл
+        job->reply(mime.name().toLatin1(), file);
+    }
 };
 
 #endif // DRAWINGS_HANDLER_H
--- cpp-server/src/main.cpp
+++ cpp-server/src/main.cpp
@@ -1,7 +1,7 @@
 // main.cpp - Главный файл приложения
 #include <QApplication>
 #include <QWebEngineView>
-#include <QWebEngineSettings> // <-- вот этот
+#include <QWebEngineSettings>
 #include <QWebChannel>
 #include <QFile>
 #include <QDir>
@@ -9,24 +9,54 @@
 #include "backend.h"
 #include <QWebEngineProfile>
 #include "drawings_handler.h"
+#include <QWebEngineUrlScheme>
 
 int main(int argc, char *argv[]) {
+    qDebug() << "Запуск приложения...";
+    
     // Для работы с Qt WebEngine необходимы эти параметры
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-    QWebEngineProfile::defaultProfile()->installUrlSchemeHandler(
-        "drawings", new DrawingsSchemeHandler()
-    );
+    
+    qDebug() << "Регистрация обработчика схемы drawings://...";
+
+    QWebEngineUrlScheme drawingsScheme("drawings");
+drawingsScheme.setFlags(QWebEngineUrlScheme::SecureScheme | 
+                       QWebEngineUrlScheme::LocalScheme |
+                       QWebEngineUrlScheme::LocalAccessAllowed);
+QWebEngineUrlScheme::registerScheme(drawingsScheme);
+
+QWebEngineProfile::defaultProfile()->installUrlSchemeHandler(
+    "drawings", new DrawingsSchemeHandler()
+);
+    
+    qDebug() << "Инициализация приложения Qt...";
     // Инициализация приложения Qt
     QApplication app(argc, argv);
     
+    // Проверка существования директории для рисунков
+    QDir drawingsDir("./drawings");
+    if (!drawingsDir.exists()) {
+        qDebug() << "Директория drawings не существует, создаём...";
+        if (drawingsDir.mkpath(".")) {
+            qDebug() << "Директория drawings успешно создана";
+        } else {
+            qWarning() << "Не удалось создать директорию drawings!";
+        }
+    } else {
+        qDebug() << "Директория drawings существует.";
+    }
+    
     // Создаем экземпляр WebEngineView - компонент для отображения веб-содержимого
+    qDebug() << "Создание WebEngineView...";
     QWebEngineView view;
     
     // Создаем экземпляр нашего бэкенда
+    qDebug() << "Создание бэкенда...";
     Backend *backend = new Backend();
     
     // Создаем канал для общения между C++ и JavaScript
+    qDebug() << "Создание WebChannel...";
     QWebChannel *channel = new QWebChannel();
     
     // Регистрируем объект backend для доступа из JavaScript
@@ -40,28 +70,30 @@ int main(int argc, char *argv[]) {
     
 #ifdef QT_DEBUG
     path = "http://localhost:8080";
+    qDebug() << "Режим отладки: загрузка из" << path;
 #else
     // В релизном режиме используем собранные файлы Vue.js из директории dist
     path = QDir::current().filePath("frontend/dist/index.html");
     QFile file(path);
     if (!file.exists()) {
         qWarning() << "Файл не найден:" << path;
-        path = "qrc:/frontend/dist/index.html";
+        path = "qrc:/vue-client/dist/index.html";
+        qDebug() << "Пробуем загрузить из ресурсов:" << path;
     } else {
         path = QUrl::fromLocalFile(path).toString();
+        qDebug() << "Загрузка из локального файла:" << path;
     }
 #endif
     
     // Загружаем Vue.js приложение
+    qDebug() << "Загрузка приложения Vue.js...";
     view.load(QUrl(path));
-
-
-
-
     
     // Устанавливаем размеры окна
     view.resize(1024, 768);
+    qDebug() << "Отображение окна...";
     view.show();
     
+    qDebug() << "Запуск главного цикла событий...";
     return app.exec();
 }
